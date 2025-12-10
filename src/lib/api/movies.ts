39c5ee1b309eb344bb35f06432209bba8fe1da2c
Namig <namig.pashayev1@gmail.com>
2025-12-10 16:27:24 +0400

    Create fetch functions to get different movies data

---
+++ src/lib/api/movies.ts
@@ -0,0 +1,53 @@
+import { MoviesResponse } from "@/types/movies";
+import { tmdbFetch } from "./client";
+
+// Popular movies
+export async function getPopularMovies() {
+    try {
+        await new Promise(res => setTimeout(res, 2000)); // simulate latency
+        const res = await tmdbFetch<MoviesResponse>("/movie/popular");
+
+        if (!res.results?.length) {
+            throw new Error("Empty 'popular' movies response");
+        }
+
+        return res.results;
+    } catch (err) {
+        console.error("getPopularMovies() failed:", err);
+        return [];
+    }
+}
+
+// Top rated movies
+export async function getTopRatedMovies() {
+    try {
+        await new Promise(res => setTimeout(res, 1200)); // simulate latency
+        const res = await tmdbFetch<MoviesResponse>("/movie/top_rated");
+
+        if (!res.results?.length) {
+            throw new Error("Empty 'top rated' movies response");
+        }
+
+        return res.results;
+    } catch (err) {
+        console.error("getTopRatedMovies() failed:", err);
+        return getPopularMovies();
+    }
+}
+
+// Upcoming movies
+export async function getUpcomingMovies() {
+    try {
+        await new Promise(res => setTimeout(res, 1200)); // simulate latency
+        const res = await tmdbFetch<MoviesResponse>("/movie/upcoming");
+
+        if (!res.results?.length) {
+            throw new Error("Empty 'upcoming' movies response");
+        }
+
+        return res.results;
+    } catch (err) {
+        console.error("getUpcomingMovies() failed:", err);
+        return [];
+    }
+}
---
+++ src/lib/api/moviesByRegion.ts
@@ -0,0 +1,46 @@
+import { MoviesResponse } from "@/types/movies";
+import { tmdbFetch } from "./client";
+import { getPopularMovies } from "./movies";
+
+// Popular movies by region
+export async function getPopularMoviesByRegion(region?: string) {
+    try {
+        const finalRegion = region ?? "US";
+        const res = await tmdbFetch<MoviesResponse>(
+            `/movie/popular?region=${finalRegion}`
+        );
+
+        await new Promise(res => setTimeout(res, 1500)); // simulate latency
+
+        if (!res.results?.length) {
+            throw new Error(`Empty 'popular' movies for region: ${finalRegion}`);
+        }
+
+        return res.results;
+    } catch (err) {
+        console.error("getPopularMoviesByRegion() failed:", err);
+        return getPopularMovies();
+    }
+}
+
+// Top rated movies by region
+export async function getTopRatedMoviesByRegion(region?: string) {
+    try {
+        const finalRegion = region ?? "US";
+
+        const res = await tmdbFetch<MoviesResponse>(
+            `/movie/top_rated?region=${finalRegion}`
+        );
+
+        await new Promise(res => setTimeout(res, 1000)); // simulate latency
+
+        if (!res.results?.length) {
+            throw new Error(`Empty 'top rated' movies for region: ${finalRegion}`);
+        }
+
+        return res.results;
+    } catch (err) {
+        console.error("getTopRatedMoviesByRegion() failed:", err);
+        return getPopularMovies();
+    }
+}
