11107af50777c01b2a5124fb9a2ad01279b9b6f5
Namig <namig.pashayev1@gmail.com>
2025-12-10 22:08:53 +0400

    Minor changes

--- src/app/popular-movies/page.tsx
+++ src/app/popular-movies/page.tsx
@@ -2,15 +2,15 @@ import { getRegion } from "@/lib/api/getRegion";
 import MovieList from "./components/MovieList";
 import { getPopularMovies, getTopRatedMovies, getUpcomingMovies } from "@/lib/api/movies";
 import { Suspense } from "react";
-import LoadingFallback from "./components/LoadingFallback";
 import { getPopularMoviesByRegion } from "@/lib/api/moviesByRegion";
+import LoadingFallback from "@/lib/components/shared/LoadingFallback";
 
 
 export default function PopularMoviesPage() {
     const regionPromise = getRegion();
 
     return (
-        <main className="bg-black min-h-screen p-15 text-white font-bold">
+        <main className="bg-black min-h-screen p-15 text-white font-bold max-sm:p-5">
             <h1 className="text-3xl border-b-2 pb-8">Explore amazing movies</h1>
 
             <Suspense fallback={<LoadingFallback>Loading popular movies globally...</LoadingFallback>}>
